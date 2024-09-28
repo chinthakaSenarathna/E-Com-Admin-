fa711cd09432c14ed2cad4677698d52b4b0fca56
chinthaka senarathna <chironprabath@gmail.com>
2024-09-28 12:43:10 +0530

    abc{{'@'}}gmail.com

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
