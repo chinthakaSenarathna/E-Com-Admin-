285d089adcbd67401b32be84baa72f358f154ea5
chinthaka senarathna <chironprabath@gmail.com>
2024-09-29 14:30:58 +0530

    FormGroup ???

--- src/app/components/products/inner-pages/new-product/new-product.component.ts
+++ src/app/components/products/inner-pages/new-product/new-product.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
+import { ProductService } from '../../../../service/product/product.service';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-new-product',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [MatDialogModule,ReactiveFormsModule],
   templateUrl: './new-product.component.html',
   styleUrl: './new-product.component.css'
 })
 export class NewProductComponent {
+
+  constructor(private productService:ProductService){}
+
+  // model
+  form = new FormGroup({
+    qty: new FormControl(0,[Validators.required]),
+    unitPrice: new FormControl(0,[Validators.required]),
+    description: new FormControl('',[Validators.required])
+  });
+
+  create(){
+    const obj = {
+      qty: this.form.value.qty,
+      unitPrice: this.form.value.unitPrice,
+      description: this.form.value.description
+    }
+
+    this.productService.create(obj)
+  }
 }
