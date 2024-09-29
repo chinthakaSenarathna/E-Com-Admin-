c3c8c290fe108ed8a14a5f0312916386e3061dfe
chinthaka senarathna <chironprabath@gmail.com>
2024-09-29 18:05:04 +0530

    set up the interceptor -> HttpManager

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { httpManagerInterceptor } from './interceptor/http-manager.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideAnimationsAsync(), provideAnimationsAsync()
+    provideHttpClient(withInterceptors([httpManagerInterceptor])),
+    provideAnimationsAsync(), 
+    provideAnimationsAsync()
   ]
 };
