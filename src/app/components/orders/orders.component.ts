910bf44da692b9702f53fd2898b15c61d9d0e3c1
chinthaka senarathna <chironprabath@gmail.com>
2024-09-29 12:57:51 +0530

    done the customer's order management structure part ??

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
