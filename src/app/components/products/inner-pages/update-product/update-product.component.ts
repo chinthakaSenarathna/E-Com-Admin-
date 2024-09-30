746639504ce5e396655aba97e468155c15cc2bfa
chinthaka senarathna <chironprabath@gmail.com>
2024-09-30 20:24:59 +0530

    fill the current data, when update the product ???

--- src/app/components/products/inner-pages/update-product/update-product.component.ts
+++ src/app/components/products/inner-pages/update-product/update-product.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ProductService } from '../../../../service/product/product.service';
 
@@ -11,9 +11,10 @@ import { ProductService } from '../../../../service/product/product.service';
   templateUrl: './update-product.component.html',
   styleUrl: './update-product.component.css'
 })
-export class UpdateProductComponent {
+export class UpdateProductComponent implements OnInit{
   readonly dialogRef = inject(MatDialogRef<UpdateProductComponent>);
-  readonly productService = inject(ProductService)
+  readonly productService = inject(ProductService);
+  readonly data = inject<any>(MAT_DIALOG_DATA);
 
   form = new FormGroup({
     qty: new FormControl(0,[Validators.required]),
@@ -21,13 +22,21 @@ export class UpdateProductComponent {
     description: new FormControl('',[Validators.required])
   });
 
+  ngOnInit(): void {
+      this.form.patchValue({
+        qty: this.data.qty,
+        unitPrice: this.data.unitPrice,
+        description: this.data.description
+      });
+  }
+
   update(){
     const obj = {
       qty: this.form.value.qty,
       unitPrice: this.form.value.unitPrice,
       description: this.form.value.description
     }
-    this.productService.update(obj,'7104f48f-24d3-4f18-839e-f7495b514508').subscribe(response => {
+    this.productService.update(obj,this.data.propertyId).subscribe(response => {
       this.dialogRef.close(true);
     }, error => {
       console.log(error?.error?.message);
